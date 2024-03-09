88bb07c256ca0f845b20bd082364bdbcdd9bed90
GabrielEscudillo <gabrielescudillo@gmail.com>
2024-03-09 14:16:48 +0100

    implemented routing functionality by adding the react router and added persistane using redux-persist

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import persistStore from 'redux-persist/es/persistStore'
+import store from './app/store.js'
+
+
+const persistor = persistStore(store)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
 )
+

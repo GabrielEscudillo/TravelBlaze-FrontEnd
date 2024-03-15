349349c77f7c98c932a82c8462963cf001696cee
GabrielEscudillo <gabrielescudillo@gmail.com>
2024-03-15 20:11:16 +0100

    updated allBookings Design

--- src/pages/AllBookings/AllBookings.jsx
+++ src/pages/AllBookings/AllBookings.jsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 import { userData } from "../userSlice";
 import { bringAllBookings, removeBooking } from "../../Services/apiCalls";
 import { Button, Card } from "react-bootstrap";
-
+import { FaTrash } from "react-icons/fa";
 
 export const AllBookings = () => {
   const [bookings, setBookings] = useState([]);
@@ -21,80 +21,95 @@ export const AllBookings = () => {
 
   const removeButtonHandler = (id) => {
     removeBooking(token, id).then(() => {
-      setBookings(bookings.filter((booking) => booking.id !== id)); 
+      setBookings(bookings.filter((booking) => booking.id !== id));
     });
   };
 
   console.log(bookings);
-  return (        
-    <div className="bookings-container">
-      <h2>My Bookings</h2>
-      {bookings ? (
-        bookings.length > 0 ? (
-          <div className="card-container">
-            {bookings.map((booking) => (
-              <div key={booking.id} className="booking-card">
-                <Card className="mb-3">
-                  <Card.Body>
-                    <Card.Title className="mb-2">Booking ID: {booking.id}</Card.Title>
-                    <Card.Text className="mb-2">Date of Purchase: {booking.date_of_purchase}</Card.Text>
-                    <Card.Text className="mb-2">Price: {booking.price}</Card.Text>
+  return (
+<div className="container my-4"> {/* Agrega margen arriba y abajo */}
+  <h2 className="text-center mb-4">My Bookings</h2>
+  {bookings ? (
+    bookings.length > 0 ? (
+      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+        {bookings.map((booking) => (
+          <div key={booking.id} className="col mb-3">
+            <div className="card h-100" id="booking-card">
+              <div className="card-body text-center">
+                <h5 className="card-title mb-2">
+                  Booking ID: {booking.id}
+                </h5>
+                <p className="card-text mb-2">
+                  Date of Purchase: {booking.date_of_purchase}
+                </p>
+                <p className="card-text mb-2">Price: {booking.price}</p>
+                <hr className="my-2" /> {/* Linea separadora */}
+                <div className="row align-items-start mb-2">
+                  <div className="col-6">
                     {booking.flight && (
                       <div>
-                        <h4 className="mb-2">Flight Details</h4>
-                        <Card.Text className="mb-2">Airline: {booking.flight.airline}</Card.Text>
-                        <Card.Text className="mb-2">Flight Number: {booking.flight.flight_number}</Card.Text>
-                        <Card.Text className="mb-2">Name: {booking.user.name}</Card.Text>
-                        <Card.Text className="mb-2">Last name: {booking.user.last_name}</Card.Text>
-                        <Button
-                          variant="danger"
-                          size="sm"
-                          onClick={() => removeButtonHandler(booking.id)}
-                        >
-                          Delete
-                        </Button>
+                        <h6 className="card-subtitle mb-2">Flight Details</h6>
+                        <p className="card-text mb-2">
+                          Airline: {booking.flight.airline}
+                        </p>
+                        <p className="card-text mb-2">
+                          Flight Number: {booking.flight.flight_number}
+                        </p>
+                        <p className="card-text mb-2">
+                          Name: {booking.user.name}
+                        </p>
+                        <p className="card-text mb-2">
+                          Last name: {booking.user.last_name}
+                        </p>
                       </div>
                     )}
+                  </div>
+                  <div className="col-6">
                     {booking.hotel && (
                       <div>
-                        <h4 className="mb-2">Hotel Details</h4>
-                        <Card.Text className="mb-2">Hotel Name: {booking.hotel.hotel_name}</Card.Text>
-                        <Card.Text className="mb-2">Name: {booking.user.name}</Card.Text>
-                        <Card.Text className="mb-2">Last name: {booking.user.last_name}</Card.Text>
-                        <Button
-                          variant="danger"
-                          size="sm"
-                          onClick={() => removeButtonHandler(booking.id)}
-                        >
-                          Delete
-                        </Button>
+                        <h6 className="card-subtitle mb-2">Hotel Details</h6>
+                        <p className="card-text mb-2">
+                          Hotel Name: {booking.hotel.hotel_name}
+                        </p>
+                        <p className="card-text mb-2">
+                          Name: {booking.user.name}
+                        </p>
+                        <p className="card-text mb-2">
+                          Last name: {booking.user.last_name}
+                        </p>
                       </div>
                     )}
-                    {booking.cruise && (
-                      <div>
-                        <h4 className="mb-2">Cruise Details</h4>
-                        <Card.Text className="mb-2">Cabin: {booking.cruise.cabin}</Card.Text>
-                        <Card.Text className="mb-2">Route: {booking.cruise.route}</Card.Text>
-                        <Button
-                          variant="danger"
-                          size="sm"
-                          onClick={() => removeButtonHandler(booking.id)}
-                        >
-                          Delete
-                        </Button>
-                      </div>
-                    )}
-                  </Card.Body>
-                </Card>
+                  </div>
+                </div>
+                {booking.cruise && (
+                  <div>
+                    <h6 className="card-subtitle mb-2">Cruise Details</h6>
+                    <p className="card-text mb-2">
+                      Cabin: {booking.cruise.cabin}
+                    </p>
+                    <p className="card-text mb-2">
+                      Route: {booking.cruise.route}
+                    </p>
+                  </div>
+                )}
+                <button
+                  className="btn btn-danger btn-sm"
+                  onClick={() => removeButtonHandler(booking.id)}
+                >
+                  <FaTrash /> Delete
+                </button>
               </div>
-            ))}
+            </div>
           </div>
-        ) : (
-          <p>No bookings found</p>
-        )
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
-);
+        ))}
+      </div>
+    ) : (
+      <p>No bookings found</p>
+    )
+  ) : (
+    <p>Loading...</p>
+  )}
+</div>
+
+  );
 };

c4b97296d7c58d2769bcbfef060a0a4696bc9053
GabrielEscudillo <gabrielescudillo@gmail.com>
2024-03-16 22:44:31 +0100

    updated register design

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -6,6 +6,14 @@ import { useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import { userSignUp, userLogin } from "../../Services/apiCalls";
 import "./Register.css";
+import {
+  FaEnvelope,
+  FaLock,
+  FaMapMarkedAlt,
+  FaPhone,
+  FaUser,
+  FaUserAlt,
+} from "react-icons/fa";
 
 export const Register = () => {
   const [signUpData, setSignUpData] = useState({
@@ -69,91 +77,138 @@ export const Register = () => {
   };
 
   return (
-    <Container id="Body" className="d-flex justify-content-center mt-5" style={{ maxWidth: "500px", margin: "auto"}}>
-    <Form onSubmit={handleSubmit} className="w-100" id="signUpBox">
+    <Container className="d-flex justify-content-center mt-5">
       <Row className="mb-2">
-        <Form.Group as={Col} controlId="formName">
-          <Form.Label>Name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter name"
-            name="name"
-            value={signUpData.name}
-            onChange={inputHandler}
-          />
-        </Form.Group>
+        <Col xs={12} md={6} className="text-center mb-4">
+          <div>
+            <img
+              src="https://i.pinimg.com/originals/27/0d/14/270d1417bac18a63b02ea5ab337a8165.gif"
+              alt="Travel Image"
+              className="mt-1 img-fluid"
+              style={{
+                maxWidth: "350px",
+                width: "100%",
+                boxShadow: "5px 5px 10px rgba(0, 0, 0, 0.2)",
+                borderRadius: "10px",
+              }}
+            />
+            <p className="mt-3">
+              Start your extraordinary journey with TravelBlaze today! Uncover
+              new horizons, embrace diverse cultures, and create memories that
+              will last a lifetime. Let your adventure begin!
+            </p>
+          </div>
+        </Col>
+        <Col xs={12} md={5} className="mb-5">
+          <Form
+            onSubmit={handleSubmit}
+            className="w-100"
+            id="SignUpBox"
+            >
+            <h3 className="mt-2 text-center">Join Our Community</h3>
 
-        <Form.Group as={Col} controlId="formLastName">
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter last name"
-            name="last_name"
-            value={signUpData.last_name}
-            onChange={inputHandler}
-          />
-        </Form.Group>
+            <Row className="mb-2">
+              <Form.Group as={Col} controlId="formName">
+                <Form.Label>
+                  <FaUser /> Name
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter name"
+                  name="name"
+                  value={signUpData.name}
+                  onChange={inputHandler}
+                />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formLastName">
+                <Form.Label>
+                  <FaUserAlt /> Last Name
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter last name"
+                  name="last_name"
+                  value={signUpData.last_name}
+                  onChange={inputHandler}
+                />
+              </Form.Group>
+            </Row>
+
+            <Form.Group className="mb-3" controlId="formAddress">
+              <Form.Label>
+                <FaMapMarkedAlt /> Address
+              </Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter address"
+                name="address"
+                value={signUpData.address}
+                onChange={inputHandler}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formEmail">
+              <Form.Label>
+                <FaEnvelope /> Email
+              </Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                name="email"
+                value={signUpData.email}
+                onChange={inputHandler}
+              />
+            </Form.Group>
+
+            <Row className="mb-3">
+              <Col>
+                <Form.Group controlId="formPassword">
+                  <Form.Label>
+                    <FaLock /> Password
+                  </Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Enter password"
+                    name="password"
+                    value={signUpData.password}
+                    onChange={inputHandler}
+                  />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="formPhoneNumber">
+                  <Form.Label>
+                    <FaPhone /> Phone Number
+                  </Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter phone number"
+                    name="phone_number"
+                    value={signUpData.phone_number}
+                    onChange={inputHandler}
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+
+            {/* Mensaje de error si no han completado todos los campos */}
+            {showError && (
+              <Alert
+                variant="danger"
+                onClose={() => setShowError(false)}
+                dismissible
+              >
+                Please fill in all fields
+              </Alert>
+            )}
+
+            <Button variant="primary" type="submit" className="w-100">
+              Register
+            </Button>
+          </Form>
+        </Col>
       </Row>
-
-      <Form.Group className="mb-3" controlId="formAddress">
-        <Form.Label>Address</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter address"
-          name="address"
-          value={signUpData.address}
-          onChange={inputHandler}
-        />
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formEmail">
-        <Form.Label>Email</Form.Label>
-        <Form.Control
-          type="email"
-          placeholder="Enter email"
-          name="email"
-          value={signUpData.email}
-          onChange={inputHandler}
-        />
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Enter password"
-          name="password"
-          value={signUpData.password}
-          onChange={inputHandler}
-        />
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formPhoneNumber">
-        <Form.Label>Phone Number</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter phone number"
-          name="phone_number"
-          value={signUpData.phone_number}
-          onChange={inputHandler}
-        />
-      </Form.Group>
-
-      {/* mensaje de error si no han completado todos los campos */}
-      {showError && (
-        <Alert
-          variant="danger"
-          onClose={() => setShowError(false)}
-          dismissible
-        >
-          Please fill in all fields
-        </Alert>
-      )}
-
-      <Button variant="primary" type="submit" className="w-30">
-        Register
-      </Button>
-    </Form>
-  </Container>
+    </Container>
   );
 };
